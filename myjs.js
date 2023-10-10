9b8cf087e7662b4d46617f8d2b184a07c839e1c5
Osamu Sugimoto <sam@josai.ac.jp>
2023-10-10 09:52:33 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,3 +5,8 @@ alert(Math.sqrt(1+1));
 alert(Math.sqrt(2)+Math.sqrt(3));​
 alert(1/0);
 alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
